f8cff4b7e6dfea81f55de4b4a946d57fc757c24e
whh <wanghaohan0530@163.com>
2024-10-15 13:55:58 +0800

    课程分页查询业务代码开发

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/model/test.java
+++
@@ -1,7 +0,0 @@
-package com.xuecheng.base.model;/**
- * @Author: whh
- * @Description: TODO  
- * @Date:    2024/10/11 下午4:12
- */ 
-public class test {
-}
---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoServiceImpl.java
@@ -0,0 +1,53 @@
+package com.xuecheng.content.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.mapper.CourseBaseMapper;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Author: whh
+ * @Description: TODO
+ * @Date: 2024/10/14 下午5:42
+ */
+@Service
+public class CourseBaseInfoServiceImpl implements CourseBaseInfoService{
+    @Autowired
+    CourseBaseMapper courseBaseMapper;
+
+    @Override
+    public PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto) {
+
+
+        //构建查询条件对象
+        LambdaQueryWrapper<CourseBase> queryWrapper = new LambdaQueryWrapper<>();
+        //构建查询条件，根据课程名称查询
+        queryWrapper.like(StringUtils.isNotEmpty(queryCourseParamsDto.getCourseName()),CourseBase::getName,queryCourseParamsDto.getCourseName());
+        //构建查询条件，根据课程审核状态查询
+        queryWrapper.eq(StringUtils.isNotEmpty(queryCourseParamsDto.getAuditStatus()),CourseBase::getAuditStatus,queryCourseParamsDto.getAuditStatus());
+//构建查询条件，根据课程发布状态查询
+//todo:根据课程发布状态查询
+
+        //分页对象
+        Page<CourseBase> page = new Page<>(pageParams.getPageNo(), pageParams.getPageSize());
+        // 查询数据内容获得结果
+        Page<CourseBase> pageResult = courseBaseMapper.selectPage(page, queryWrapper);
+        // 获取数据列表
+        List<CourseBase> list = pageResult.getRecords();
+        // 获取数据总数
+        long total = pageResult.getTotal();
+        // 构建结果集
+        PageResult<CourseBase> courseBasePageResult = new PageResult<>(list, total, pageParams.getPageNo(), pageParams.getPageSize());
+        return courseBasePageResult;
+
+
+    }
+}
